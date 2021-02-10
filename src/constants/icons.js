b6dbcd16f02e66c0ae19f0e12eb154170cf68dc2
Markaa10 <rahulrajdahal@gmail.com>
2021-02-10 19:11:07 +0545

    navbar

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,3 +1,5 @@
 import Logo from "../../assets/icons/logo.svg";
+import Menu from "../../assets/icons/menu.svg";
+import Chat from "../../assets/icons/chat.svg";
 
-export { Logo };
+export { Logo, Menu, Chat };
--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,9 +1,61 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { COLORS } from "../constants";
+import { Logo, Menu, Chat } from "../constants/icons";
 
 const Dashboard = () => {
+  function renderNavbar() {
+    return (
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          marginTop: 36,
+        }}
+      >
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            marginRight: 228.08,
+            alignSelf: "flex-start",
+          }}
+        >
+          <Menu style={{ marginRight: 14 }} />
+          <Logo width="33.59" height="24" />
+        </View>
+        <View
+          style={{
+            width: 10,
+            height: 10,
+            backgroundColor: COLORS.danger,
+            borderRadius: 10,
+            bottom: 9,
+            left: 27,
+            zIndex: 1,
+          }}
+        />
+        <Chat style={{ alignSelf: "flex-end" }} />
+      </View>
+    );
+  }
+
   return (
-    <View>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: COLORS.white,
+        paddingHorizontal: 24,
+      }}
+    >
+      {/* Navbar  */}
+      {renderNavbar()}
       <Text>Dashboard</Text>
     </View>
   );
