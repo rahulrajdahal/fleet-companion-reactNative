b457dd696c699d285f3fc5848856c4e66d8227a4
Markaa10 <rahulrajdahal@gmail.com>
2021-03-11 18:49:27 +0545

    notification settings

--- src/screens/profile/Notifications.js
+++ src/screens/profile/Notifications.js
@@ -41,12 +41,91 @@ const Notifications = () => {
     );
   }
 
+  function renderNoficationSettings() {
+    const notificationSettings = [
+      {
+        _id: 1,
+        title: "New Job Notification",
+        body: "We will send you a notification once new jobs are posted",
+      },
+      {
+        _id: 2,
+        title: "Message Notification",
+        body: "We will send you a notification wherever someone messages you.",
+      },
+      {
+        _id: 3,
+        title: "Announcements",
+        body: "We will send you a notification for important announcements.",
+      },
+      {
+        _id: 4,
+        title: "Reports",
+        body: "Receive notification about your submitted reports instantly.",
+      },
+    ];
+
+    return (
+      <View>
+        <Text
+          style={{
+            color: COLORS.text400,
+            ...FONTS.caption,
+            fontFamily: "Manrope",
+            fontWeight: "600",
+            marginTop: 40,
+            marginBottom: 16,
+          }}
+        >
+          Get Notifications for:
+        </Text>
+
+        {notificationSettings.map((notificationSetting) => (
+          <View
+            key={notificationSetting._id}
+            style={{
+              flexDirection: "row",
+              marginBottom: 24,
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <View style={{ width: 200 }}>
+              <Text
+                style={{
+                  color: COLORS.text700,
+                  ...FONTS.body3,
+                  fontWeight: "bold",
+                  fontFamily: "Manrope",
+                }}
+              >
+                {notificationSetting.title}
+              </Text>
+              <Text
+                style={{
+                  color: COLORS.text600,
+                  ...FONTS.caption,
+                  lineHeight: 16,
+                  marginTop: 4,
+                }}
+              >
+                {notificationSetting.body}
+              </Text>
+            </View>
+            <ToggleButton />
+          </View>
+        ))}
+      </View>
+    );
+  }
+
   return (
     <View style={{ flex: 1, backgroundColor: COLORS.white }}>
       {/* Push Notification */}
       {renderPushNotification()}
 
-      <ToggleButton style={{ width: 60, height: 32 }} />
+      {/* Notification Settings */}
+      {renderNoficationSettings()}
     </View>
   );
 };
