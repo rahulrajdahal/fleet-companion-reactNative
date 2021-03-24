381fe204935cb5452c25647cf332ce9bc8fda12f
Markaa10 <rahulrajdahal@gmail.com>
2021-03-24 19:34:33 +0545

    toptaboptoins

--- src/navigations/TopTabNavigator.js
+++ src/navigations/TopTabNavigator.js
@@ -8,28 +8,23 @@ import { Active } from "../constants/icons";
 const Tab = createMaterialTopTabNavigator();
 
 const TopTabNavigator = ({ modalVisible }) => {
+  const tabBarOptions = {
+    style: {
+      elevation: 0,
+      backgroundColor: modalVisible ? "rgba(2, 14, 38, 0.15)" : COLORS.white,
+    },
+    activeTintColor: COLORS.text800,
+    labelStyle: { ...FONTS.h6, textTransform: "capitalize" },
+    inactiveTintColor: COLORS.text500,
+    showIcon: true,
+    iconStyle: { top: 34, left: 73 },
+    indicatorStyle: {
+      backgroundColor: modalVisible ? "rgba(2, 14, 38, 0.15)" : COLORS.white,
+    },
+  };
+
   return (
-    <Tab.Navigator
-      initialRouteName="ActiveWork"
-      tabBarOptions={{
-        style: {
-          elevation: 0,
-          backgroundColor: modalVisible
-            ? "rgba(2, 14, 38, 0.15)"
-            : COLORS.white,
-        },
-        activeTintColor: COLORS.text800,
-        labelStyle: { ...FONTS.h6, textTransform: "capitalize" },
-        inactiveTintColor: COLORS.text500,
-        showIcon: true,
-        iconStyle: { top: 34, left: 73 },
-        indicatorStyle: {
-          backgroundColor: modalVisible
-            ? "rgba(2, 14, 38, 0.15)"
-            : COLORS.white,
-        },
-      }}
-    >
+    <Tab.Navigator initialRouteName="ActiveWork" tabBarOptions={tabBarOptions}>
       <Tab.Screen
         name="ActiveWork"
         children={() => <ActiveWork modalVisible={modalVisible} />}
